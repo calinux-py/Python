a5475c05616b3c3684bff43eb83d87dbf64ece4d
CaliNux <132522491+calinux-py@users.noreply.github.com>
2023-08-07 21:11:59 -0700

    Add files via upload

---
+++ Subnet Device Grab/SubnetGrab.py
@@ -0,0 +1,85 @@
+import os
+import socket
+import netifaces
+import scapy.all as scapy
+import tempfile
+import sys
+import requests
+
+WEBHOOK_URL = WEBHOOK_URL
+
+
+def get_current_subnet():
+    # Get the default network interface to determine the current subnet
+    default_interface = netifaces.gateways()['default'][netifaces.AF_INET][1]
+    interfaces = netifaces.interfaces()
+
+    # Get the IP and netmask of the default interface
+    if default_interface in interfaces:
+        addrs = netifaces.ifaddresses(default_interface)
+        ip_info = addrs[netifaces.AF_INET][0]
+        ip_address = ip_info['addr']
+        netmask = ip_info['netmask']
+        return ip_address, netmask
+    else:
+        return None
+
+
+def map_subnet_devices(ip_address, netmask):
+    subnet_devices = []
+    subnet_range = '.'.join([str(int(ip_address.split('.')[i]) & int(netmask.split('.')[i])) for i in range(4)])
+    subnet_cidr = subnet_range + '/' + str(sum(bin(int(x)).count('1') for x in netmask.split('.')))
+
+    # Use ARP requests to get device information within the subnet
+    ans, _ = scapy.arping(subnet_cidr, timeout=2, verbose=False)
+    for _, rcv in ans.res:
+        device_name = socket.getfqdn(rcv.psrc)
+        subnet_devices.append((device_name, rcv.psrc))
+
+    return subnet_devices
+
+
+def upload_to_discord(webhook_url, file_path):
+    with open(file_path, 'rb') as file:
+        payload = {"file": (file_path, file)}
+        response = requests.post(webhook_url, files=payload)
+        if response.status_code == 200:
+            print("File uploaded successfully to Discord.")
+        else:
+            print(f"Failed to upload the file to Discord. Status code: {response.status_code}")
+
+
+def main():
+    # Get the current subnet
+    subnet_info = get_current_subnet()
+
+    if subnet_info:
+        ip_address, netmask = subnet_info
+
+        # Create a temporary file to store the output
+        temp_dir = tempfile.gettempdir()
+        output_file = os.path.join(temp_dir, "subnet_devices.txt")
+
+        with open(output_file, 'w') as f:
+            sys.stdout = f  # Redirect stdout to the file
+
+            print(f"Current Subnet: {ip_address}/{netmask}")
+
+            # Map devices on the current subnet
+            subnet_devices = map_subnet_devices(ip_address, netmask)
+            print("\nDevices on the Subnet:")
+            for device_name, device_ip in subnet_devices:
+                print(f"Device Name: {device_name}, IP Address: {device_ip}")
+
+        sys.stdout = sys.__stdout__
+
+        print(f"Output written to: {output_file}")
+
+        # Upload the file to Discord
+        upload_to_discord(WEBHOOK_URL, output_file)
+    else:
+        print("Unable to retrieve subnet information.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Subnet Device Grab/readme.txt
@@ -0,0 +1,43 @@
+# Subnet Scanner and Discord Uploader
+
+This Python script allows you to scan the devices connected to your local network subnet and upload the results to a Discord channel using a webhook. The script utilizes `netifaces`, `scapy`, and `requests` modules to achieve this functionality.
+
+## Installation
+
+To install the required modules, use the following pip command:
+
+```bash
+pip install netifaces scapy requests
+```
+
+Please make sure you have Python installed on your system before running the installation command.
+
+## Configuration
+
+Before running the script, you need to set up your Discord webhook URL. Replace the placeholder `"YOUR DISCORD WEBHOOK HERE"` in the script with your actual webhook URL. The script will use this URL to upload the scan results to your designated Discord channel.
+
+## How to Use
+
+1. Open a terminal or command prompt.
+
+2. Navigate to the directory where you saved the Python script.
+
+3. Run the script using Python:
+
+```bash
+python subnet_scanner_discord.py
+```
+
+4. The script will scan your local network subnet, identify the devices connected to it, and generate a file named `subnet_devices.txt` in your system's temporary directory. The output will also be displayed on the console.
+
+5. The script will then upload the `subnet_devices.txt` file to the configured Discord channel using the webhook URL.
+
+6. You can check your Discord channel to view the uploaded file and see the list of devices on your network subnet.
+
+## Important Note
+
+Please ensure that you have the necessary permissions to upload files to the Discord channel using the webhook. If you encounter any issues during the script execution or upload process, check your Discord webhook settings and permissions.
+
+## Disclaimer
+
+This script is intended for educational and informational purposes only. Use it responsibly and only on networks you have authorization to scan. The author and contributors are not responsible for any misuse or illegal activities conducted with this script.
